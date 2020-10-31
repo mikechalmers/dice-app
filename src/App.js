50d9ded23dd5df95394a69fa964539585a49d41d
Mike Chalmers <mikechalmers@gmail.com>
2020-10-31 13:39:30 +0000

    add some logic and initial HeroQuest styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { Component } from 'react';
 import RollDice from './RollDice';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hi
-      <RollDice />
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        dice: 2
+    };
+}
+  render() {
+    return (
+      <div className="App">
+      <h1>HeroQuest Dice</h1>
+      <RollDice dice={ this.state.dice } />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import './RollDice.css';
 
 class RollDice extends Component{
+    static defaultProps = {
+        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+    }
     constructor(props){
         super(props);
         this.state = {
-
+            roll: "six"
         };
     }
     render(){
         return(
-            <div>
-                <Die roll="one" />
-                <Die roll="two" />
+            <div className="RollDice">
+                <div className="RollDice-dice">
+                    <Die roll={ this.state.roll } />
+                    <Die roll={ this.state.roll } />
+                </div>
                 <button>Roll ya dice</button>
             </div>
         );
