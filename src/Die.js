f817faba0dc65ad2bdb449579a8cb16859e8cd6e
Mike Chalmers <mikechalmers@gmail.com>
2020-11-01 11:01:00 +0000

    refactor to first working version

--- src/Die.js
+++ src/Die.js
@@ -3,10 +3,12 @@ import './Die.css';
 
 class Die extends Component{
     render(){
-        let dieClass = "fas fa-dice-" + this.props.roll; 
+        let dieClass = "fas fa-dice-" + this.props.roll;
+        let dieSource = "/images/HeroQuest-Die-Face-" + this.props.result + ".png";
         return(
             <div className="Die">
-                <i className={`${dieClass} ${ this.props.rolling ? 'roll-shake' : ''}`}></i>
+                {/* <i className={`${dieClass} ${ this.props.rolling ? 'roll-shake' : ''}`}></i> */}
+                <img src={`${dieSource}`} className={`${dieClass} ${ this.props.rolling ? 'roll-shake' : ''}`} alt=""/>
             </div>
         );
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -4,21 +4,43 @@ import './RollDice.css';
 
 class RollDice extends Component{
     static defaultProps = {
-        sides: ['one', 'two', 'three', 'four', 'five', 'six']
+        faces: ['1', '2', '3', '4', '5', '6']
     }
     constructor(props){
         super(props);
         this.state = {
             roll: "six",
+            result1: 6,
+            result2: 6,
+            result3: 6,
+            result4: 6,
+            result5: 6,
+            result6: 6,
             numberOfDice: 2,
             rolling: false
         };
     }
     rollEm = (e) => {
+        
+        const newDie1 = Math.floor((Math.random() * 6) + 1);
+        const newDie2 = Math.floor((Math.random() * 6) + 1);
+        const newDie3 = Math.floor((Math.random() * 6) + 1);
+        const newDie4 = Math.floor((Math.random() * 6) + 1);
+        const newDie5 = Math.floor((Math.random() * 6) + 1);
+        const newDie6 = Math.floor((Math.random() * 6) + 1);
+        
         this.setState({ rolling: true });
 
         setTimeout(() => {
-            this.setState({ rolling: false })
+            this.setState({ 
+                rolling: false,
+                result1: newDie1,
+                result2: newDie2,
+                result3: newDie3,
+                result4: newDie4,
+                result5: newDie5,
+                result6: newDie6
+            })
         }, 1000)
     }
 
@@ -43,11 +65,12 @@ class RollDice extends Component{
     render(){
         
         let diceArray = [];
-
+        let results = [this.state.result1, this.state.result2, this.state.result3, this.state.result4, this.state.result5, this.state.result6];
+        console.log(results);
         for(let i = 0; i < this.state.numberOfDice; i++){
-            const randomSide = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+            const randomResult = results[i];
             diceArray.push(
-                <Die roll={randomSide} rolling={this.state.rolling} />
+                <Die result={randomResult} rolling={this.state.rolling} />
             )
         }
 
