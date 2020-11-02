6b4a19a6e23094f89ec7836cc8a1666a2293a147
Mike Chalmers <mikechalmers@gmail.com>
2020-11-02 11:42:28 +0000

    fix margin sizes on mobile

--- src/RollDice.js
+++ src/RollDice.js
@@ -77,7 +77,7 @@ class RollDice extends Component{
         return(
             <div className="RollDice">
                 <div className="container">
-                    <div className="row my-5 justify-content-center">
+                    <div className="row my-5 my-sm-2 justify-content-center">
                         { diceArray }
                     </div>
                 </div>
